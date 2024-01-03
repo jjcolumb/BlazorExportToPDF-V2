0b0fdcd789c94b8cd7e928ffdc78be15e50e235b
Jose Javier Columbie <josejaviercolumbie@yahoo.com>
2024-01-03 10:56:48 -0700

    Add project files.

---
+++ BlazorExportToPDF/Data/WeatherForecastService.cs
@@ -0,0 +1,49 @@
+namespace BlazorExportToPDF.Data
+{
+    public class WeatherForecastService
+    {
+        private List<WeatherForecast> Forecast { get; set; }
+
+        private static string[] CloudCover = new[] {
+        "Sunny", "Partly cloudy", "Cloudy", "Storm"
+    };
+
+        Tuple<int, string>[] ConditionsForForecast = new Tuple<int, string>[] {
+        Tuple.Create( 22 , "Hot"),
+        Tuple.Create( 13 , "Warm"),
+        Tuple.Create( 0 , "Cold"),
+        Tuple.Create( -10 , "Freezing")
+    };
+
+        public WeatherForecastService()
+        {
+            Forecast = CreateForecast();
+        }
+
+        private List<WeatherForecast> CreateForecast()
+        {
+            var rng = new Random();
+            DateTime startDate = DateTime.Now;
+            return Enumerable.Range(1, 30).Select(index => {
+                var temperatureC = rng.Next(-10, 30);
+                return new WeatherForecast
+                {
+                    Date = startDate.AddDays(index),
+                    TemperatureC = temperatureC,
+                    CloudCover = CloudCover[rng.Next(0, 4)],
+                    Forecast = ConditionsForForecast.First(c => c.Item1 <= temperatureC).Item2
+                };
+            }).ToList();
+        }
+
+        public List<WeatherForecast> GetForecast()
+        {
+            return Forecast;
+        }
+
+        public Task<WeatherForecast[]> GetForecastAsync(CancellationToken ct = default)
+        {
+            return Task.FromResult(Forecast.ToArray());
+        }
+    }
+}
---
+++ BlazorExportToPDF/wwwroot/js/script.js
@@ -0,0 +1,8 @@
+﻿window.BlazorDownloadFile = function (filename, data, contentType) {
+    const a = document.createElement('a');
+    a.href = `data:${contentType};base64,${data}`;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
